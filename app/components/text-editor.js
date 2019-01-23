c768e4bd7db27cf67e321c7ac00ac4feef490481
Etienne Baqué <etiennebaque@gmail.com>
2019-01-22 23:10:40 -0500

    Introduced markdown preview

--- app/components/text-editor.js
+++ app/components/text-editor.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  formValues: {},
+  writtenText: 'This is an example',
+
+  preview: computed('writtenText', function() {
+    return this.get('writtenText');
+  }),
 });
