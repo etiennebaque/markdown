23e74bf3f9ca92b4b40a8b776eb89db3a390143d
Etienne Baqué <ebaque@financeit.io>
2019-01-23 12:56:34 -0500

    Added behavior for preview update

--- app/components/text-editor.js
+++ app/components/text-editor.js
@@ -4,8 +4,20 @@ import { computed } from '@ember/object';
 export default Component.extend({
   formValues: {},
   writtenText: 'This is an example',
+  updatedText: 'This is an example',
 
-  preview: computed('writtenText', function() {
-    return this.get('writtenText');
+  preview: computed('writtenText', 'updatedText', function() {
+    if (this.get('formValues.liveChange')) {
+      return this.get('writtenText');
+    } else {
+      return this.get('updatedText');
+    } 
   }),
+
+  actions: {
+    updatePreview(event) {
+      event.preventDefault();
+      this.set('updatedText', this.get('writtenText'));
+    }
+  }
 });
