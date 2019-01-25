5774d09f0d2a096f35bf567838aece0c076a5fc5
Etienne Baqué <ebaque@financeit.io>
2019-01-24 19:05:13 -0500

    Added disabled style to button

--- app/components/text-editor.js
+++ app/components/text-editor.js
@@ -14,6 +14,16 @@ export default Component.extend({
     } 
   }),
 
+  buttonState: computed('formValues.liveChange', function() {
+    var cls = 'text-md px-4 py-2 rounded-lg text-white mt-4 lg:mt-0 '
+    if (this.get('formValues.liveChange')) {
+      cls = cls + 'bg-grey-darkest opacity-50 cursor-not-allowed'
+    } else{
+      cls = cls + 'bg-blue hover:bg-blue-darker';
+    }
+    return cls;
+  }),
+
   actions: {
     updatePreview(event) {
       event.preventDefault();
