f55e253635ccc226658eb741fe3dfd3ee3f869b5
Etienne Baqué <ebaque@financeit.io>
2019-01-31 17:03:44 -0500

    Fixed a few things

--- app/components/text-editor.js
+++ app/components/text-editor.js
@@ -2,21 +2,20 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  formValues: {},
+  liveChange: false,
   writtenText: 'This is an example',
   updatedText: '',
 
-  preview: computed('writtenText', 'updatedText', function() {
-    if (this.get('formValues.liveChange')) {
-      return this.get('writtenText');
-    } else {
-      return this.get('updatedText');
+  preview: computed('writtenText', 'updatedText', 'liveChange', function() {
+    if (this.get('liveChange')) {
+      this.send('updatePreview');
     } 
+    return this.get('updatedText');
   }),
 
-  buttonState: computed('formValues.liveChange', function() {
+  buttonState: computed('liveChange', function() {
     var cls = 'text-md px-4 py-2 rounded-lg text-white mt-4 lg:mt-0 '
-    if (this.get('formValues.liveChange')) {
+    if (this.get('liveChange')) {
       cls = cls + 'bg-grey-darkest opacity-50 cursor-not-allowed'
     } else{
       cls = cls + 'bg-blue hover:bg-blue-darker';
@@ -26,7 +25,6 @@ export default Component.extend({
 
   actions: {
     updatePreview(event) {
-      event.preventDefault();
       this.set('updatedText', this.get('writtenText'));
     }
   }
